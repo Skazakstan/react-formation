23c3758078152f8a906121e961ff6a8f7209b957
Skazakstan <40268328+Skazakstan@users.noreply.github.com>
2025-03-25 15:49:36 +0100

    downArrow

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,22 +1,36 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const navLinks = [
   { name: "Login", href: "/login" },
   { name: "Register", href: "/register" },
   { name: "Forgot password ?", href: "/forgot-password" },
 ];
+
 export default function AuthLayout({
+  // layout definition with children as node
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
   return (
     <>
       <ul>
         {navLinks.map((link, index) => {
+          const isActive = pathname.startsWith(link.href);
+
           return (
             <li key={index}>
-              <Link href={link.href}>{link.name}</Link>
+              <Link
+                href={link.href}
+                className={
+                  isActive ? "text-red-600 font-bold" : "text-slate-500"
+                }
+              >
+                {link.name}
+              </Link>
             </li>
           );
         })}
