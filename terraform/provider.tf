62cbd70d32c544db04c2978ef605f1f0c90e18ff
Skazakstan <40268328+Skazakstan@users.noreply.github.com>
2025-03-31 18:21:34 +0200

    fiix acm region

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,4 +8,18 @@ provider "aws" {
       ManagedBy   = "Terraform"
     }
   }
-} 
+}
+
+# Provider spécifique pour la région us-east-1 (nécessaire pour ACM avec CloudFront)
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Project     = var.project_name
+      ManagedBy   = "Terraform"
+    }
+  }
+}
